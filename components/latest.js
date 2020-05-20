3afeecae42053d3257b9890fde8f7cc3af510ff5
Daniel McKeown <2019@sf3am.com>
2020-05-19 21:26:37 -0700

    update CSS in latest component for better readability; add The Iliad

--- components/latest.js
+++ components/latest.js
@@ -33,7 +33,7 @@ function latest() {
         {item.map(function(ibook) {
           console.log(ibook);
           return(
-          <li key={ibook.bookTitle}><span className="bookTitle">{ibook.bookTitle}</span> <span className="divider">|</span> <span id="author">{ibook.bookAuthor ? ibook.bookAuthor : '(author missing)'}</span> <span className="divider">|</span> <span id="author">{ibook.bookYear ? ibook.bookYear : '(year missing)'}</span> <span className="link"><a href={urlPermalinkBase + ibook.locator}>link</a></span> · <span className="download"><a href={urlBase + ibook.vFile}>download</a></span></li>
+          <li key={ibook.bookTitle}><span className="bookTitle">{ibook.bookTitle}</span> <span className="divider">|</span> <span className="author">{ibook.bookAuthor ? ibook.bookAuthor : '(author missing)'}</span> <span className="divider">|</span> <span className="year">{ibook.bookYear ? ibook.bookYear : '(year missing)'}</span> <span className="link"><a href={urlPermalinkBase + ibook.locator}>link</a></span> · <span className="download"><a href={urlBase + ibook.vFile}>download</a></span></li>
           )
         })}
       </ul>
@@ -50,10 +50,29 @@ function latest() {
           font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
           font-size: calc(1.314rem);
         }
-        ul#booksCollection li {
+        div#latestBooks ul#booksCollection li {
           margin-bottom: calc(1vh + 5pt);
           font-family:  -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
           font-size: calc(1.127rem);
+        }
+        div#latestBooks span.divider {
+          color: azure;
+        }
+        div#latestBooks a {
+          color: #4e5ba6;
+          font-size: calc(0.95rem);
+        }
+        div#latestBooks a:visited {
+          color: #6e73e6;
+        }
+        div#latestBooks a:hover {
+          color: #e6806a;
+        }
+        div#latestBooks span.author {
+          color: #57585c;
+        }
+        div#latestBooks span.year {
+          color: #7c8da1;
         }
       `}</style>
     </div>
